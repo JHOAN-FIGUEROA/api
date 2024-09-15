390471443ce5efb6dfcce33737e6677d50e521f8
JHOAN-FIGUEROA <163602603+JHOAN-FIGUEROA@users.noreply.github.com>
2024-09-15 11:05:32 -0500

    Update Cliente.js

--- models/Cliente.js
+++ models/Cliente.js
@@ -1,13 +1,10 @@
 const mongoose = require('mongoose');
 
 const clienteSchema = new mongoose.Schema({
-  nombre: String,
-  apellido: String,
-  correo: String,
-  telefono: String,
-  
-    }],
-  }],
+  nombre: { type: String, required: true },
+  apellido: { type: String, required: true },
+  correo: { type: String, required: true, unique: true },
+  telefono: { type: String, required: true },
 });
 
 const Cliente = mongoose.model('Cliente', clienteSchema);
