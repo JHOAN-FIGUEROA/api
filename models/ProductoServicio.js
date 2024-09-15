19cf1a0bc7b33d265934b3c38da1ebf083d9403f
JHOAN-FIGUEROA <163602603+JHOAN-FIGUEROA@users.noreply.github.com>
2024-09-14 20:50:59 -0500

    Update ProductoServicio.js

--- models/ProductoServicio.js
+++ models/ProductoServicio.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose'); 
 const productoServicioSchema = new mongoose.Schema({
-    _id: String,
-    nombre: String,
-    descripcion: String,
-    precio: Number,
-    tipo: String
+  nombre: { type: String, required: true },
+  categoria: { type: String, required: true },
+  cantidad: { type: Number, required: true },
+  precio: { type: Number, required: true }, // Asegúrate de que esto sea de tipo Number
+  fechaIngreso: { type: Date, required: true },
+  anulado: { type: Boolean, default: false },
   });
   
   const ProductoServicio = mongoose.model('ProductoServicio', productoServicioSchema);
