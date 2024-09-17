ba4a1710fc62edc5374fc1661327f017c9d563e5
JHOAN-FIGUEROA <163602603+JHOAN-FIGUEROA@users.noreply.github.com>
2024-09-16 21:50:00 -0500

    Update Compra.js

--- models/Compra.js
+++ models/Compra.js
@@ -1,1 +1,37 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
+const ProductoServicioSchema = new Schema({
+  producto_servicio_id: {
+    type: String,
+    required: true,
+  },
+  nombre: {
+    type: String,
+    required: true,
+  },
+  precio: {
+    type: Number,
+    required: true,
+  },
+});
+
+const CompraSchema = new Schema({
+  proveedor_id: {
+    type: String,
+    required: true,
+  },
+  fecha: {
+    type: Date,
+    required: true,
+  },
+  total: {
+    type: Number,
+    required: true,
+  },
+  productos_servicios: [ProductoServicioSchema],
+});
+
+const Compra = mongoose.model('Compra', CompraSchema);
+
+module.exports = Compra;
