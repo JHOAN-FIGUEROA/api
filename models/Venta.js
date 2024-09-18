d4f7052c22b92c1d5a53b378d3ba46fd218b2719
JHOAN-FIGUEROA <163602603+JHOAN-FIGUEROA@users.noreply.github.com>
2024-09-18 11:24:21 -0500

    Update Venta.js

--- models/Venta.js
+++ models/Venta.js
@@ -1,19 +1,22 @@
 const mongoose = require('mongoose'); 
 const ventaSchema = new mongoose.Schema({
-    _id: String,
-    cliente_id: String,
-    fecha: Date,
-    total: Number,
-    estado: { type: String, default: 'activa' },
-    productos_servicios: [
-      {
-        producto_servicio_id: String,
-        nombre: String,
-        precio: Number
-      }
-    ]
-  });
-  
+     
+  cliente: { type: String, required: true },
+  fecha: { type: Date, required: true },
+  total: { type: Number, required: true },
+   estado: { type: String, default: 'activa' },
+},
+  productos_servicios: [
+    {
+      producto_servicio_id: String,
+      nombre: String,
+      precio: Number,
+      cantidad : Number,
+    }
+  ]
+});
+
+module.exports = mongoose.model('Venta', ventaSchema);
   const Venta = mongoose.model('Venta', ventaSchema);
   module.exports = Venta;
   
