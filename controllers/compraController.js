c4397c78ffd2634a0817c3fc2dcade8b5f2c2342
JHOAN-FIGUEROA <163602603+JHOAN-FIGUEROA@users.noreply.github.com>
2024-09-19 07:52:37 -0500

    Update compraController.js

--- controllers/compraController.js
+++ controllers/compraController.js
@@ -54,13 +54,13 @@ exports.getCompraById = async (req, res) => {
 // Crear nueva compra
 exports.createCompra = async (req, res) => {
     try {
-        const { proveedor, fecha, total, estado, productos_servicios } = req.body;
+        const {  fecha, total, estado, productos_servicios } = req.body;
 
         
 
         const compra = new Compra({
             
-            proveedor,
+            
             fecha,
             total,
             estado: estado || 'completado',
