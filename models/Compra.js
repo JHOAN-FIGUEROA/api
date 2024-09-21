2aaf0f78a0bad3711aa3c83a19ab3357d51cadb5
JHOAN-FIGUEROA <163602603+JHOAN-FIGUEROA@users.noreply.github.com>
2024-09-20 20:18:21 -0500

    Update Compra.js

--- models/Compra.js
+++ models/Compra.js
@@ -1,21 +1,20 @@
 const mongoose = require('mongoose');
 
 const compraSchema = new mongoose.Schema({
-    
-    proveedor: { type: String,},
+    proveedor: { type: String, required: true },
     fecha: { type: Date, required: true },
     total: { type: Number, required: true },
     estado: {
         type: String,
-        enum: ['pendiente', 'cancelado', 'completado'],
+        enum: ['completado'],
         default: 'completado'
     },
     productos_servicios: [
         {
-           
-            nombre: String,
-            precio: Number,
-            cantidad: Number,
+            producto_servicio_id: { type: mongoose.Schema.Types.ObjectId, ref: 'ProductoServicio', required: true },
+            nombre: { type: String, required: true },
+            precio: { type: Number, required: true },
+            cantidad: { type: Number, required: true },
         }
     ]
 });
